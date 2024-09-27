c153837058d0c8de3becbf070a71d916d3a0b000
Peter B. Golbus <pgolbus@gmail.com>
2024-09-27 11:36:35 -0400

    giving them a different thing

--- HW/HW3 Design/wildlife_tracker/animal_management/animal_manager.py
+++ HW/HW3 Design/wildlife_tracker/animal_management/animal_manager.py
@@ -1,5 +1,17 @@
 from typing import Optional
 
+from wildlife_tracker.animal_managment.animal import Animal
+
 class AnimalManager:
 
-    pass
+    def __init__(self) -> None:
+        animals: dict[int, Animal] = {}
+
+    def get_animal_by_id(self, animal_id: int) -> Optional[Animal]:
+        pass
+
+    def register_animal(Animal) -> None:
+        pass
+
+    def remove_animal(animal_id: int) -> None:
+        pass
