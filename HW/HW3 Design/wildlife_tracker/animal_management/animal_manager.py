7224be9981341543166893b191aea47a02bc0551
Nuo Chen <1936405561@qq.com>
2024-10-08 02:11:29 +0000

    testing

--- HW/HW3 Design/wildlife_tracker/animal_management/animal_manager.py
+++ HW/HW3 Design/wildlife_tracker/animal_management/animal_manager.py
@@ -10,7 +10,7 @@ class AnimalManager:
     def get_animal_by_id(self, animal_id: int) -> Optional[Animal]:
         pass
 
-    def register_animal(Animal) -> None:
+    def register_animal(self, Animal) -> None:
         pass
 
     def remove_animal(animal_id: int) -> None:
