a803fce527d9667f104b62c41207ff78adee9a93
toletich311 <toletich@bu.edu>
2025-02-07 12:06:52 -0500

    changed files to work with mac and added imports to app.py

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
+
 def hello():
     response = make_response(
         {
@@ -16,4 +18,5 @@ if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', debug=True)
+    osVar = os.getenv("PORT", default=None)
+    app.run(host='0.0.0.0',port=osVar, debug=True)
