03c0790aea69f67897ea2018d10edf7e8023e3e8
minjie zuo <minjiez@bu.edu>
2025-02-07 16:40:55 -0500

    Add screenshots and updates for hw1-flask

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, make_response, request
+import time
 
 app = Flask(__name__)
 
@@ -15,7 +16,7 @@ def hello():
 
 @app.route('/repeat')
 def repeat_input():
-    user_input = request.args.get('input', 'No input provided')
+    user_input = request.args.get('input', 'No input')
     response = make_response(
         {
             'body': user_input,
@@ -24,10 +25,30 @@ def repeat_input():
     )
     return response
 
+@app.route('/health')
+
+def health():
+    response = make_response(
+        {
+            'body': 'OK',
+            'status': 200
+        }
+    )
+    return response
+
+@app.route('/healthcheck')
+def healthcheck():
+    return {'status': 'healthy'}
+
+
+@app.route('/hang')
+def hang():
+    while True:
+        time.sleep(1)
 
 if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(host='0.0.0.0',port=5000, debug=True, threaded=False)
  #
