1c9bd28a58f9e92a2a7656a3d75a917e405c1d2c
falktravis <tfalk@Travis-Laptop.>
2025-02-09 17:53:59 -0500

    step 3 complete

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -23,9 +23,20 @@ def repeat():
         )
     return response
 
+@app.route('/health')
+@app.route('/healthcheck')
+def health():
+    response = make_response(
+            {
+                'response': "OK",
+                'status': 200
+            }
+        )
+    return response
+
 
 if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=os.getenv('PORT'), debug=True)
