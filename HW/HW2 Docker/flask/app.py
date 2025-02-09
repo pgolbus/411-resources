32d12bf345dc335130f58568399c17ac96a10a29
esteeg <esteeg@bu.edu>
2025-02-09 15:44:40 -0500

    removed redundant 'if __main__' statement

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask, make_response, request
 
 app = Flask(__name__)
@@ -36,10 +35,15 @@ def health():
     )
     return response
 
+@app.route('/hang')
+def hang():
+    while True:
+        pass
+
 if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    if __name__ == '__main__':
-        port = int(os.getenv("PORT", 5000))
-        app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False, threaded=False)
+
