d32ac95ef65b130276bb9b654d53d3ecc2d4bed6
toletich311 <toletich@bu.edu>
2025-02-10 17:35:02 -0500

    Route W/ Get Parameter

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,9 +1,15 @@
-from flask import Flask, make_response, request
+from flask import Flask, make_response,jsonify, request
 import os
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/repeat', methods=['GET'])
+
+def repeat():
+    user_input = request.args.get('input', '')  # Get the 'input' parameter or use an empty string if missing
+    return jsonify({"body": user_input, "status": 200})
+
 
 def hello():
     response = make_response(
@@ -18,5 +24,5 @@ if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    osVar = os.getenv("PORT", default=None)
+    osVar = os.getenv("PORT", 5002)
     app.run(host='0.0.0.0',port=osVar, debug=True)
