d32dbad95725cdbc66097399fa15fbc2fa461578
AdiSingh998 <asingh4@bu.edu>
2025-02-09 20:54:08 -0500

    did the first few steps for flask and  dockerfile

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, make_response
+import os 
 
 app = Flask(__name__)
+port = int(os.getenv("PORT", 5000))
 
 @app.route('/')
 def hello():
@@ -16,4 +18,5 @@ if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', debug=True)
+    
+    app.run(host='0.0.0.0', port=port)
