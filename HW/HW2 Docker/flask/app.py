0621178a9b12559602d249fb86f14eb05eed63ae
Martin <soy26@bu.edu>
2025-02-10 10:52:41 +0800

    Completed step 2
    Added a route that uses a get parameter and uploaded screenshots

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 
 app = Flask(__name__)
 
@@ -13,9 +13,20 @@ def hello():
     )
     return response
 
+@app.route('/repeat', methods=['GET'])
+def repeat():
+    input_value = request.args.get('input', '')
+    response = make_response(
+        {
+            'body': input_value,
+            'status': 200
+        }
+    )
+    return response
+
 if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
