6a86dce348f64217a79e2935b2a7094e6cb680b2
DylanConnol <dylanconnolly2004@gmail.com>
2025-02-12 16:53:19 -0500

    Updated flask and dockerfile

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, make_response
+import os
 
+PORT = os.getenv('PORT')
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,4 +18,4 @@ if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, port=PORT)
