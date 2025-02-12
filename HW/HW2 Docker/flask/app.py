ca5c535aa95e6396ee8766e6e2413158a8bbc0f0
Davin Martin <35785346+red4dj@users.noreply.github.com>
2025-02-12 15:08:13 -0500

    HW2: Flask

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
+from os import getenv
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
@@ -12,8 +14,38 @@ def hello():
     )
     return response
 
+
+@app.route('/repeat')
+def repeat():
+    response = make_response(
+        {
+            'response': request.args['input'],
+            'status': 200
+        }
+    )
+    return response
+
+
+@app.route('/health')
+@app.route('/healthcheck')
+def health():
+    response = make_response(
+        {
+            'response': 'OK',
+            'status': 200
+        }
+    )
+    return response
+
+
+@app.route('/hang')
+def hang():
+    while True:
+        pass
+
+
 if __name__ == '__main__':
-    # By default flask is only accessible from localhost.
+    # By default, flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=getenv('PORT'), debug=True, threaded=False)
