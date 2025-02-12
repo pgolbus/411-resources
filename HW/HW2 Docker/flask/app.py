6806c326df8eed78ec224ccbe685ae17f5fd7d40
brendanmcoyne <bmcoyne@bu.edu>
2025-02-12 16:00:16 -0500

    Finished flask copy pasta

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, make_response, request
 import os
+import time 
 
 app = Flask(__name__)
+
+# Step 1 #
 
 @app.route('/')
 def hello():
@@ -12,6 +15,8 @@ def hello():
         }
     )
     return response
+
+# Step 2 #
 
 @app.route('/repeat')
 def repeat():
@@ -24,9 +29,26 @@ def repeat():
     )
     return response
 
+# Step 3 #
+
+@app.route('/health')
+@app.route('/healthcheck')
+def health():
+    response = make_response(
+        {
+            'response': 'OK',
+            'status': 200
+        }
+    )
+    return response
+
+# Step 4 #
+
+@app.route('/hang')
+def hang():
+    while True:
+        time.sleep(1)
+        
 if __name__ == '__main__':
-    # By default flask is only accessible from localhost.
-    # Set this to '0.0.0.0' to make it accessible from any IP address
-    # on your network (not recommended for production use)
     port = int(os.getenv("PORT", 5002)) 
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, threaded=False) 
