6051921ead4b94cacd0a4bfcdaf8880337765d36
KunyuanLi <lky0409@bu.edu>
2025-02-11 22:04:59 -0500

    Updated Flask app with additional functionality and healthcheck

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -24,5 +24,8 @@ def health():
     return jsonify({"health": "good"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5001))  # Default to 5001 instead of 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
