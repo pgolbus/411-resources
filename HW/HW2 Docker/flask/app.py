81ebc11d88060af534fd59c00d2c8222b7b49819
Leo Rava <leor@bu.edu>
2025-02-12 16:08:35 -0500

    Changes for dockerfile and app.py for step 4 of flask section

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -27,8 +27,24 @@ def repeat_input():
     )
     return response
 
+@app.route('/health')
+@app.route('/healthcheck')
+def health():
+    response = make_response(
+        {
+            'body': 'OK',
+            'status': 200
+        }
+    )
+    return response
+
+@app.route('/hang')
+def hang():
+    while True:
+        pass
+
 if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port = port, debug=True, threaded=False)
