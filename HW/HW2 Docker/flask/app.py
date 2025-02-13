09cfef43b19a22b4b5b03f7230ea9c6250822ddf
Varsha <varsha_athreya@yahoo.com>
2025-02-12 22:37:57 -0500

    app and dockerfile changes

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,6 +1,34 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request, jsonify
+import os
 
+port = os.getenv('PORT')
 app = Flask(__name__)
+
+@app.route('/hang', methods=['GET'])
+def hang():
+    while True:
+        pass
+
+@app.route('/repeat', methods=['GET'])
+def repeat():
+    input = request.args.get('input')
+    return jsonify(
+        {
+            "body": input,
+            "status": 200
+        }
+    )
+
+@app.route('/health')
+@app.route('/healthcheck')
+def health():
+    response = make_response(
+        {
+            'body': 'OK',
+            'status': 200
+        }
+    )
+    return response
 
 @app.route('/')
 def hello():
@@ -16,4 +44,7 @@ if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, port=port, threaded=False)
+
+
+
