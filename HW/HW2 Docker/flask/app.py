e7d0d7b70b2179786221f1b9acb0bff49eb95ca8
WillWin07 <winarnowill@gmail.com>
2025-02-14 01:14:49 -0800

    nice

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,21 +1,27 @@
-from flask import Flask, make_response
-import os
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    response = make_response(
-        {
-            'response': 'Hello, World!',
-            'status': 200
-        }
-    )
-    return response
+    return jsonify(response="Hello, World!", status=200)
+
+@app.route('/repeat')
+def repeat():
+    input_value = request.args.get('input', '')
+    return jsonify(body=input_value, status=200)
+
+@app.route('/health')
+@app.route('/healthcheck')
+def health():
+    return jsonify(body="OK", status=200)
+
+@app.route('/hang')
+def hang():
+    while True:
+        pass  
 
 if __name__ == '__main__':
-    # By default flask is only accessible from localhost.
-    # Set this to '0.0.0.0' to make it accessible from any IP address
-    # on your network (not recommended for production use)
-    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
-    app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, threaded=False)
