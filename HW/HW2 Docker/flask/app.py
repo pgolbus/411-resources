132ab3d2d9b3f3a20d22adc45904574b94a9a853
WillWin07 <winarnowill@gmail.com>
2025-02-14 00:47:41 -0800

    initial commit

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, make_response
+import os
 
 app = Flask(__name__)
 
@@ -16,4 +17,5 @@ if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
