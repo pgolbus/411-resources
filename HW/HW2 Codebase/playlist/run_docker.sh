a13ecb73dc24099e5453f4c4c4ceaf0c182fb4fe
Peter B. Golbus <pgolbus@gmail.com>
2025-02-26 11:08:20 -0500

    removing too helpful scripts

--- HW/HW2 Codebase/playlist/run_docker.sh
+++ HW/HW2 Codebase/playlist/run_docker.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 # Variables
-IMAGE_NAME="playlist"
-CONTAINER_TAG="0.0.1"
-HOST_PORT=5000
-CONTAINER_PORT=5000
-DB_VOLUME_PATH="./db"   # Adjust this to the desired host path for the database persistence
-BUILD=true  # Set this to true if you want to build the image
+IMAGE_NAME=
+CONTAINER_TAG=
+HOST_PORT=
+CONTAINER_PORT=
+DB_VOLUME_PATH=
+BUILD=
+
+# This is an example of accessing a variable
+# Notice lack of whitespace around the = sign
+FOO=Foo
+# The {} are not necessary but are there for disambiguation in the variable name
+# It's safer to just use them
+echo ${FOO}
 
 # Check if we need to build the Docker image
 if [ "$BUILD" = true ]; then
   echo "Building Docker image..."
-  docker build -t ${IMAGE_NAME}:${CONTAINER_TAG} .
 else
   echo "Skipping Docker image build..."
 fi
@@ -19,18 +25,16 @@ fi
 # Check if the database directory exists; if not, create it
 if [ ! -d "${DB_VOLUME_PATH}" ]; then
   echo "Creating database directory at ${DB_VOLUME_PATH}..."
-  mkdir -p ${DB_VOLUME_PATH}
 fi
 
 # Stop and remove the running container if it exists
+# This assumes the name of the container is ${IMAGE_NAME}_container
 if [ "$(docker ps -q -a -f name=${IMAGE_NAME}_container)" ]; then
     echo "Stopping running container: ${IMAGE_NAME}_container"
-    docker stop ${IMAGE_NAME}_container
 
     # Check if the stop was successful
     if [ $? -eq 0 ]; then
         echo "Removing container: ${IMAGE_NAME}_container"
-        docker rm ${IMAGE_NAME}_container
     else
         echo "Failed to stop container: ${IMAGE_NAME}_container"
         exit 1
@@ -40,12 +44,8 @@ else
 fi
 
 # Run the Docker container with the necessary ports and volume mappings
+# Ma
 echo "Running Docker container..."
-docker run -d \
-  --name ${IMAGE_NAME}_container \
-  --env-file .env \
-  -p ${HOST_PORT}:${CONTAINER_PORT} \
-  -v ${DB_VOLUME_PATH}:/app/db \
-  ${IMAGE_NAME}:${CONTAINER_TAG}
+
 
 echo "Docker container is running on port ${HOST_PORT}."
