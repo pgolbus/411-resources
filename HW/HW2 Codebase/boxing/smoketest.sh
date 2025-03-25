e56cd7838042940c4c29d654dff05147cf9e62e7
wesleyyjpark <wyjpark@bu.edu>
2025-03-25 18:24:08 -0400

    added smoketest

---
+++ HW/HW2 Codebase/boxing/smoketest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define the base URL for the Flask API
+BASE_URL="http://localhost:5001/api"
+
+# Flag to control whether to echo JSON output
+ECHO_JSON=false
+
+# Parse command-line arguments
+while [ "$#" -gt 0 ]; do
+  case $1 in
+    --echo-json) ECHO_JSON=true ;;
+    *) echo "Unknown parameter passed: $1"; exit 1 ;;
+  esac
+  shift
+done
+
+###############################################
+#
+# Health checks
+#
+###############################################
+
+
