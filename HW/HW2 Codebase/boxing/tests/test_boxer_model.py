588cb4a0ab002947dda9956c2a30b2a31eab8b73
jle122 <jle4231@gmail.com>
2025-04-02 11:39:56 -0400

    Added some more stubbed unit tests to boxer_model, as well as actual unit test for duplicate boxer

--- HW/HW2 Codebase/boxing/tests/test_boxer_model.py
+++ HW/HW2 Codebase/boxing/tests/test_boxer_model.py
@@ -2,15 +2,39 @@ from dataclasses import asdict
 
 import pytest
 
-from playlist.models.playlist_model import PlaylistModel
-from playlist.models.song_model import Song
+from boxing.models.boxers_model import Boxer
+from boxing.models.ring_model import RingModel
 
 @pytest.fixture()
 def boxer_model():
     return create_boxer()
 
+def test_addBoxer_with_Invalid_height():
+    return true
+
+def test_addBoxer_with_Invalid_weight():
+    return true
+
+def test_addBoxer_with_Invalid_reach():
+    return true
+
+def test_addBoxer_with_Invalid_age():
+    return true
+
+def test_add_duplicate_boxer(boxer_model, sample_boxer1):
+    """Test error when adding a duplicate boxer
+
+    """
+    playlist_model.add_song_to_playlist(sample_boxer1)
+    with pytest.raises(ValueError, match="Boxer with name already exists"):
+        playlist_model.add_song_to_playlist(sample_song1)
+
 @pytest.fixture()
 def delete_boxer_test():
+    return true
+
+@pytest.fixture()
+def delete_boxer_DNEinDB_test():
     return true
 
 @pytest.fixture()
@@ -18,15 +42,23 @@ def get_leaderboard_test():
     return true
 
 @pytest.fixture()
-def boxer_model():
-    return create_boxer();
+def get_leaderboard_withInvalidSort_test():
+    return true
 
 @pytest.fixture()
 def get_boxer_by_id_test():
     return true
 
 @pytest.fixture()
-def get_boxer_by_name():
+def get_boxer_by_idDNE_test():
+    return true
+
+@pytest.fixture()
+def get_boxer_by_name_test():
+    return true
+
+@pytest.fixture()
+def get_boxer_by_nameDNE_test():
     return true
 
 @pytest.fixture()
@@ -34,7 +66,17 @@ def get_weight_class():
     return true
 
 @pytest.fixture()
-def update_boxer_stats():
+def get_weight_classInvalid():
     return true
 
+@pytest.fixture()
+def update_boxer_stats_test():
+    return true
 
+@pytest.fixture()
+def update_boxer_stats_butINvalidResult():
+    return true
+
+@pytest.fixture()
+def update_boxer_stats_butBoxerDNE():
+    return true
