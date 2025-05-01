be4ce93be0a585eab4d03a9835972297bba3325d
VirKon42104 <viroopsk@bu.edu>
2025-04-30 21:47:56 -0400

    Updated smoketest file.

--- HW/FinalProject Basketball/basketballers/smoketest.py
+++ HW/FinalProject Basketball/basketballers/smoketest.py
@@ -1,40 +1,41 @@
 import requests
 
-
 def run_smoketest():
     base_url = "http://localhost:5001/api"
     username = "test"
     password = "test"
 
-    test_muhammad_ali = {
-        "name": "Muhammad Ali",
-        "weight": 210,
-        "height": 191,
-        "reach": 78,
-        "age": 32
+    test_player_1 = {
+        "full_name": "LeBron James",
+        "position": "F",
+        "team": "Los Angeles Lakers",
+        "height_feet": 6,
+        "height_inches": 9,
+        "weight_pounds": 250
     }
 
-    test_joe_frazier = {
-        "name": "Joe Frazier",
-        "weight": 205,
-        "height": 182,
-        "reach": 73,
-        "age": 30
+    test_player_2 = {
+        "full_name": "Stephen Curry",
+        "position": "G",
+        "team": "Golden State Warriors",
+        "height_feet": 6,
+        "height_inches": 2,
+        "weight_pounds": 185
     }
 
     health_response = requests.get(f"{base_url}/health")
     assert health_response.status_code == 200
     assert health_response.json()["status"] == "success"
 
-    delete_user_response = requests.delete(f"{base_url}/reset-users")
+    delete_user_response = requests.delete(f"{base_url}/delete-users")
     assert delete_user_response.status_code == 200
     assert delete_user_response.json()["status"] == "success"
-    print("Reset users successful")
+    print("Delete users successful")
 
-    delete_boxer_response = requests.delete(f"{base_url}/reset-boxers")
-    assert delete_boxer_response.status_code == 200
-    assert delete_boxer_response.json()["status"] == "success"
-    print("Reset boxers successful")
+    delete_player_response = requests.delete(f"{base_url}/delete-players")
+    assert delete_player_response.status_code == 200
+    assert delete_player_response.json()["status"] == "success"
+    print("Delete players successful")
 
     create_user_response = requests.put(f"{base_url}/create-user", json={
         "username": username,
@@ -55,11 +56,14 @@ def run_smoketest():
     assert login_resp.json()["status"] == "success"
     print("Login successful")
 
-    create_boxer_resp = session.post(f"{base_url}/add-boxer", json=test_muhammad_ali)
-    assert create_boxer_resp.status_code == 201
-    assert create_boxer_resp.json()["status"] == "success"
-    print("Boxer creation successful")
 
+     # Add player 1
+    add_player_resp = session.post(f"{base_url}/add-player", json=test_player_1)
+    assert add_player_resp.status_code == 201
+    assert add_player_resp.json()["status"] == "success"
+    print("Player 1 creation successful")
+    
+  
     # Change password
     change_password_resp = session.post(f"{base_url}/change-password", json={
         "new_password": "new_password"
@@ -77,10 +81,13 @@ def run_smoketest():
     assert login_resp.json()["status"] == "success"
     print("Login with new password successful")
 
-    create_boxer_resp = session.post(f"{base_url}/add-boxer", json=test_joe_frazier)
-    assert create_boxer_resp.status_code == 201
-    assert create_boxer_resp.json()["status"] == "success"
-    print("Boxer creation successful")
+   # Add player 2
+   
+    add_player_resp = session.post(f"{base_url}/add-player", json=test_player_2)
+    assert add_player_resp.status_code == 201
+    assert add_player_resp.json()["status"] == "success"
+    print("Player 2 creation successful")
+
 
     # Log out
     logout_resp = session.post(f"{base_url}/logout")
@@ -88,11 +95,11 @@ def run_smoketest():
     assert logout_resp.json()["status"] == "success"
     print("Logout successful")
 
-    create_boxer_logged_out_resp = session.post(f"{base_url}/add-boxer", json=test_muhammad_ali)
+    add_player_logged_out_resp = session.post(f"{base_url}/add-player", json=test_player_1)
     # This should fail because we are logged out
-    assert create_boxer_logged_out_resp.status_code == 401
-    assert create_boxer_logged_out_resp.json()["status"] == "error"
-    print("Boxer creation failed as expected")
+    assert add_player_logged_out_resp.status_code == 401
+    assert add_player_logged_out_resp.json()["status"] == "error"
+    print("Player creation failed as expected")
 
 if __name__ == "__main__":
     run_smoketest()
